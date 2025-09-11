01e44071034eb1fceef6a9dcdf759d5cc50e32ac
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-09-10 23:29:13 -0400

    changed db config, populate users table

--- backend/auth.js
+++ backend/auth.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import {sql} from './config/db.js';
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
@@ -10,9 +11,57 @@ passport.use(new GoogleStrategy({
   callbackURL: process.env.CALLBACK_URL,
   passReqToCallback: true,
 },
-function(request, accessToken, refreshToken, profile, done) {
-  return done(null, profile);
+async function(request, accessToken, refreshToken, profile, done) {
+   try
+   {
+    const name = profile.displayName;
+
+    const nameParts = name.split(" ");
+    const firstName = nameParts[0];
+    const lastName = nameParts.slice(1).join(" ") || "";
+    const email = profile.emails[0].value;
+    const photo = profile.photos[0].value;
+    // const user = {
+    //   name: name,
+    //   email: email,
+    //   photo: photo,
+    // };
+
+    // I need to check if this user already exists in the database
+    // if not, I need to create a new user in the database
+    // if they do exist, I need to return the user from the database
+    const existingUser = await sql `
+    SELECT * 
+    FROM users 
+    WHERE email = ${email}
+    `;
+
+    let user ;
+
+    // if the length of the result we just queried is 0 , then we know this must be a  new user
+    if(existingUser.length === 0)
+    {
+      const newUser = await sql `
+        INSERT INTO users 
+        (first_name,last_name,email,photo) 
+        VALUES (${firstName},${lastName},${email},${photo}) 
+        RETURNING *
+      `;
+      user = newUser[0];
+    } 
+    else 
+    {
+      user = existingUser[0];
+    }
+    
+
+    return done(null, user);
+   } catch(err)
+   {
+    return done(err,null);
+   }
 }));
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {PGHOST, PGUSER, PGPASSWORD, PGDATABASE} = process.env;
+const { DATABASE_URL } = process.env;
 
-if (!PGHOST || !PGUSER || !PGPASSWORD || !PGDATABASE) {
-  throw new Error("Database configuration variables are missing.");
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing.");
 }
 
-export const sql = neon(
-  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`);
+export const sql = neon(DATABASE_URL);
