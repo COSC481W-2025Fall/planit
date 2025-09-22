ebc24c8c226363fb231f9b7dfd289cd70171de8e
mouzhass <hmouzaih@emich.edu>
2025-09-22 14:43:12 -0400

    Added Setting page with NavBar.jsx and TopBanner.jsx intergrated with setting page
    edited App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TripPage from "./pages/TripPage";
+import SettingsPage from "./pages/SettingsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route
-        path="/trip"
-        element={
-          <ProtectedRoute>
-            <TripPage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+                path="/trip"
+                element={
+                    <ProtectedRoute>
+                        <TripPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/settings"
+                element={
+                    <ProtectedRoute>
+                        <SettingsPage />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
