81017ef726641b4d3233751d4463933696fb50ad
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-09-24 18:31:47 -0400

    remove testing pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,10 @@ import LoginPage from "./pages/LoginPage";
 import TripPage from "./pages/TripPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Test from "./pages/Test";
 function App() {
     return (
         <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path = "/testing" element = {<Test />} />
             <Route
                 path="/trip"
                 element={
