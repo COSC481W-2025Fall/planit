cd79f6ea97e8673d5c53cfd50aa5e0e3a2150f6f
mouzhass <hmouzaih@emich.edu>
2025-09-23 23:24:35 -0400

    deleted comments in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,10 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
     return (
         <Routes>
-            {/* Landing page as the root */}
             <Route path="/" element={<LandingPage />} />
 
-            {/* Login page */}
             <Route path="/login" element={<LoginPage />} />
 
-            {/* Protected Trip page */}
             <Route
                 path="/trip"
                 element={
