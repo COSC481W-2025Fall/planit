428e3974aed12aba7a8cb5f1091d84bb14d94aa9
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-10-14 19:56:32 -0400

    grab all the locations from the trips table to work on search bar

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,7 +1,7 @@
 /*
 Routes for trip-related operations: create a trip, modify a trip, read a trip, and delete a trip.
 */
-import {createTrip, readTrip, updateTrip, deleteTrip, fetchUserTrips} from "../controllers/tripController.js";
+import {createTrip, readTrip, updateTrip, deleteTrip, fetchUserTrips, getAllTripLocations} from "../controllers/tripController.js";
 import express from "express";
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.put("/update", updateTrip);
 router.get("/read/:tripId", readTrip);
 router.get("/readAll", fetchUserTrips);
 router.delete("/delete", deleteTrip);
+router.get("/all/trip/locations", getAllTripLocations);
 
 export default router;
