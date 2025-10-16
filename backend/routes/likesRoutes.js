0454dbec5985a3a3790e512d2bd74ecd77441136
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-10-15 23:57:37 -0400

    added an endpoint to make a list of all the trips the user has liked

--- backend/routes/likesRoutes.js
+++ backend/routes/likesRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { toggleLike,getAllLikesByUser} from "../controllers/likesController.js";
+import { toggleLike, getAllTripDetailsOfATripLikedByUser, getLikedTripIdsByUser} from "../controllers/likesController.js";
 
 const router = express.Router();
 
 router.post("/toggle", toggleLike);
-router.post("/all/likes/user", getAllLikesByUser);
+router.post("/all/trip/details", getAllTripDetailsOfATripLikedByUser);
+router.post("/all/tripId/liked/by/user", getLikedTripIdsByUser); 
 
 
 export default router;
