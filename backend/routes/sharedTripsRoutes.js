6b84b86a336a642e50c0a3d452f6b2dfa954d6ff
Cmliakos <cmliakos@gmail.com>
2025-10-21 17:55:32 -0400

    Added routes for endpoints related to trip sharing

--- backend/routes/sharedTripsRoutes.js
+++ backend/routes/sharedTripsRoutes.js
@@ -1,7 +1,13 @@
-import { readAllSharedTrips } from "../controllers/sharedTripsController";
 import express from "express";
+import { isLoggedIn } from "../auth.js";
+import { loadEditableTrip } from "../middleware/loadEditableTrip.js";
+import { loadOwnedTrip } from "../middleware/loadOwnedTrip.js";
+import { addParticipant, removeParticipant, listParticipants, listMySharedTrips, } from "../controllers/sharedTripsController.js";
 const router = express.Router();
 
-router.get("/readAll", readAllSharedTrips);
+router.post("/addParticipant", isLoggedIn, loadOwnedTrip, addParticipant);
+router.delete("/removeParticipant", isLoggedIn, loadOwnedTrip, removeParticipant);
+router.get("/listParticipants", isLoggedIn, loadEditableTrip, listParticipants);
+router.get("/listMySharedTrips", isLoggedIn, listMySharedTrips);
 
 export default router;
