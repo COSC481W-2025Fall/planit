a0981ebc5fe2c1fcec2e64e10a1ca116564fe3dd
Angelia Philip <180352026+ajp4417@users.noreply.github.com>
2025-11-04 00:22:02 -0500

    Fixed formatting

--- frontend/src/pages/TripPage.jsx
+++ frontend/src/pages/TripPage.jsx
@@ -429,7 +429,8 @@ export default function TripPage() {
                                       trip_name: formData.get("name"),
                                       trip_location: formData.get("location"),
                                       trip_start_date: formData.get("startDate"),
-                                      image_id: selectedImage ? selectedImage.image_id : (editingTrip?.image_id ?? 1),                                      trip_end_date: formData.get("endDate"),
+                                      image_id: selectedImage ? selectedImage.image_id : (editingTrip?.image_id ?? 1),                                      
+                                      trip_end_date: formData.get("endDate"),
                                       user_id: user.user_id,
                                       isPrivate: privacyDraft //PLACEHOLDER UNTIL FRONTEND IMPLEMENTS A WAY TO TRIGGER BETWEEN PUBLIC AND PRIVATE FOR TRIPS
                                   };
@@ -495,7 +496,7 @@ export default function TripPage() {
 
                   {/* Display selected image preview */}
                   {selectedImage && (
-                    <div style={{ marginTop: "10px" }}>
+                    <div className="selected-image-container">
                       <img
                         src={selectedImage.imageUrl}
                         alt={selectedImage.image_name}
