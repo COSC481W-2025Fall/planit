4d3da2b2d6fe4cab0c81b1a1df0f7ec41039a777
Cmliakos <cmliakos@gmail.com>
2025-11-17 02:48:27 -0500

    getParticipantLongestTrip Tests

--- backend/tests/settingsParticipantController.test.js
+++ backend/tests/settingsParticipantController.test.js
@@ -53,4 +53,54 @@ describe("Settings Participant Controller", () => {
         expect(res.json).toHaveBeenCalledWith({ error: "Database error" });
     });
 
-    
+    //getParticipantLongestTrip Tests
+
+    it("should return longest participant trip for valid userID", async () => {
+        const req = { body: { userID: 1 } };
+        const res = mockRes();
+
+        const mockTrip = [
+            { trip_name: "Vegas Trip", trip_id: 9, total_days: 6 },
+        ];
+        sql.mockResolvedValueOnce(mockTrip);
+
+        await participantController.getParticipantLongestTrip(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(mockTrip[0]);
+    });
+
+    it("should return 400 if userID is missing in getParticipantLongestTrip", async () => {
+        const req = { body: {} };
+        const res = mockRes();
+
+        await participantController.getParticipantLongestTrip(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ error: "userID is required" });
+    });
+
+    it("should return null if no participant trips exist in getParticipantLongestTrip", async () => {
+        const req = { body: { userID: 1 } };
+        const res = mockRes();
+
+        sql.mockResolvedValueOnce([]);
+
+        await participantController.getParticipantLongestTrip(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(null);
+    });
+
+    it("should return 500 if SQL fails in getParticipantLongestTrip", async () => {
+        const req = { body: { userID: 1 } };
+        const res = mockRes();
+
+        sql.mockRejectedValueOnce(new Error("DB failure"));
+
+        await participantController.getParticipantLongestTrip(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ error: "Internal Server Error" });
+    });
+
