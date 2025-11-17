c6a0e7daf9a56fdebbf655dddbef3216cb9e88d9
Cmliakos <cmliakos@gmail.com>
2025-11-17 02:41:10 -0500

    Init settings participant controller test file

---
+++ backend/tests/settingsParticipantController.test.js
@@ -0,0 +1,4 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import * as participantController from "../controllers/settingsParticipantController.js";
+import { sql } from "../config/db.js";
+
