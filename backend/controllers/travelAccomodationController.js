789b6601fec25e875c74fe58bd0e3f7ec1c20fa2
Andrew Hainsworth <ahainsw1@emich.edu>
2025-11-18 21:06:44 -0500

    Added routes for two new endpoints, addTransportInfo and addFlightInfo

---
+++ backend/controllers/travelAccomodationController.js
@@ -0,0 +1,9 @@
+import { sql } from "../config/db.js";
+
+export const addFlightInfo = async (req, res) => {
+    
+}
+
+export const addAccomodationInfo = async (req, res) => {
+    
+}
---
+++ backend/routes/travelAccomodationRoutes.js
@@ -0,0 +1,2 @@
+import express from "express"; 
+import { addTransportInfo } from "../controllers/travelAccomodationController.js";
