7ce4dd9597af32d0ae73e6989412c0eff8e68483
Andrew Hainsworth <ahainsw1@emich.edu>
2025-11-18 21:56:52 -0500

    created endpoint for adding transport object

--- backend/controllers/travelAccommodationController.js
+++ backend/controllers/travelAccommodationController.js
@@ -1,7 +1,29 @@
 import { sql } from "../config/db.js";
 
 export const addTransportInfo = async (req, res) => {
-    
+    try {
+        const {type, price, note, trip_id, number} = req.body;
+
+        if (!trip_id || !type) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const result = await sql`
+            INSERT INTO transport (trip_id, transport_type, transport_price, transport_note)
+            VALUES (${transport_type}, ${trip_id}, ${transport_price ?? null}, ${transport_note ?? null} ${transport_number ?? null})
+            RETURNING *
+        `;
+
+        res.json({
+      message: "Activity added successfully",
+      activity: created[0],
+    });
+
+    }
+    catch (err) {
+        console.error("error adding transport info:", err);
+        res.status(500).json({ error: err.message });
+    } 
 }
 
 export const addAccommodationInfo = async (req, res) => {
