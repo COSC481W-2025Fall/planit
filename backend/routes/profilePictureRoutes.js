24084d3ca4a96d1d7014c6afad89dcf3e07ef3bd
Adam Snowgold <196842243+AdamSnow12@users.noreply.github.com>
2025-11-19 14:11:26 -0500

    Delete old controller function and routing

--- backend/routes/profilePictureRoutes.js
+++
@@ -1,8 +0,0 @@
-import {storeProfilePicture, retrieveProfilePicture} from "../controllers/profilePictureController.js";
-import express from "express";
-const router = express.Router();
-
-router.put("/store", storeProfilePicture);
-router.get("/retrieve", retrieveProfilePicture);
-
-export default router;
