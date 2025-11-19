b85642c2e18c8b755e4027aec795a9bc0b3eb67f
Andrew Hainsworth <ahainsw1@emich.edu>
2025-11-19 14:45:01 -0500

    added routed for accommodation read, update, and delete

--- backend/routes/travelAccommodationRoutes.js
+++ backend/routes/travelAccommodationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"; 
-import { addTransportInfo, updateTransportInfo, readTransportInfo, deleteTransportInfo, addAccommodationInfo } from "../controllers/travelAccommodationController.js";
+import { addTransportInfo, updateTransportInfo, readTransportInfo, deleteTransportInfo, addAccommodationInfo, readAccommodationInfo, updateAccommodationInfo, deleteAccommodationInfo } from "../controllers/travelAccommodationController.js";
 
 const router = express.Router(); 
 
@@ -7,7 +7,9 @@ router.post("/addTransportInfo", addTransportInfo);
 router.get("/readTransportInfo", readTransportInfo);
 router.put("/updateTransportInfo", updateTransportInfo);
 router.delete("/deleteTransportInfo", deleteTransportInfo);
-router.post("/addAccommodationInfo", addAccomodationInfo);
+router.post("/addAccommodationInfo", addAccommodationInfo);
 router.get("/readAccommodationInfo", readAccommodationInfo);
+router.put("/updateAccommodationInfo", updateAccommodationInfo);
+router.delete("/deleteAccommodationInfo", deleteAccommodationInfo);
 
 export default router;
