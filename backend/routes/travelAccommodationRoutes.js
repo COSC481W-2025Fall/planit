01a12cba52d04b7537a94d5df26937ccb6e28a8c
Andrew Hainsworth <ahainsw1@emich.edu>
2025-11-18 22:24:58 -0500

    Created routes for new endpoints (updating transport, reading transport, deleting transport)

--- backend/routes/travelAccommodationRoutes.js
+++ backend/routes/travelAccommodationRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"; 
-import { addTransportInfo, addAccommodationInfo } from "../controllers/travelAccommodationController.js";
+import { addTransportInfo, updateTransportInfo, readTransportInfo, deleteTransportInfo, addAccommodationInfo } from "../controllers/travelAccommodationController.js";
 
 const router = express.Router(); 
 
 router.post("/addTransportInfo", addTransportInfo);
+router.get("/readTransportInfo", readTransportInfo);
+router.put("/updateTransportInfo", updateTransportInfo);
+router.delete("/deleteTransportInfo", deleteTransportInfo);
 router.post("/addAccommodationInfo", addAccomodationInfo);
 
 export default router;
